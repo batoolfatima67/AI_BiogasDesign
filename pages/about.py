96c7084bff98ffec26e49fbea9efebd81b8f4613
Batool Fatima <fatima67fgb@gmail.com>
2026-07-07 21:58:29 +0500

    Create about.py

---
+++ pages/about.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("ℹ About")
+
+st.markdown("""
+## BioDesignAI
+
+Version 1.0
+
+BioDesignAI is an open-source engineering software developed using Python and Streamlit.
+
+### Objectives
+
+- Simplify biogas plant design
+- Automate engineering calculations
+- Generate engineering drawings
+- Support sustainable energy solutions
+
+### Developed By
+
+Batool Fatima
+
+Agricultural Engineer
+
+Python Developer
+
+GIS & Remote Sensing Analyst
+""")
